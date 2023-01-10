9da1b87b5e83a527d5c2633b605f60eea9a3c708
anggitpp <anggitpp23@gmail.com>
2023-01-10 16:09:34 +0700

    add method and folder for log

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'throw' => false,
         ],
 
+        "log" => [
+            "driver" => "local",
+            "root" => storage_path("app/public/log"),
+            "url" => env("APP_URL") . "/logs",
+            "visibility" => "public",
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),

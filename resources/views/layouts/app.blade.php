5b80d7023f1d3718768b68b589411827548d9d94
anggitpp <anggitpp23@gmail.com>
2023-01-15 16:22:12 +0700

    implement layout for dashboard home page and dashboard ess

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,18 +62,22 @@
             <!--begin::Main-->
             <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
                 <!--begin::Content wrapper-->
-                <div class="d-flex flex-column flex-column-fluid">
-                    <!--begin::Toolbar-->
-                    @include('layouts.toolbar')
-                    <!--end::Toolbar-->
-                    <!--begin::Content-->
-                    <div id="kt_app_content" class="app-content flex-column-fluid">
-                        <div id="kt_app_content_container" class="app-container container-xxl">
-                            @yield('content')
+                @if(!Str::contains(url()->current(), 'dashboard') && !Str::contains(url()->current(), 'ess-dash'))
+                    <div class="d-flex flex-column flex-column-fluid">
+                        <!--begin::Toolbar-->
+                        @include('layouts.toolbar')
+                        <!--end::Toolbar-->
+                        <!--begin::Content-->
+                        <div id="kt_app_content" class="app-content flex-column-fluid">
+                            <div id="kt_app_content_container" class="app-container container-xxl">
+                                @yield('content')
+                            </div>
                         </div>
+                        <!--end::Content-->
                     </div>
-                    <!--end::Content-->
-                </div>
+                @else
+                    @yield('content')
+                @endif
                 <!--end::Content wrapper-->
                 <!--begin::Footer-->
                 @include('layouts.footer')

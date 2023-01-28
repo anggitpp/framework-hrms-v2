044756014eee987ceb0f0df2e5b4fb67157e7604
anggitpp <anggitpp23@gmail.com>
2023-01-28 15:33:25 +0700

    create repository for master data and implement payroll setting for relation model

--- app/Models/Setting/AppMasterData.php
+++ app/Models/Setting/AppMasterData.php
@@ -2,8 +2,12 @@
 
 namespace App\Models\Setting;
 
+use App\Models\Payroll\PayrollSetting;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class AppMasterData extends Model
 {
@@ -18,4 +22,19 @@ class AppMasterData extends Model
         'order',
         'status',
     ];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(AppMasterData::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(AppMasterData::class, 'parent_id');
+    }
+
+    public function payrollSetting(): HasOne
+    {
+        return $this->hasOne(PayrollSetting::class, 'reference_id');
+    }
 }
---
+++ app/Repositories/Setting/AppMasterDataRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories\Setting;
+
+use App\Models\Setting\AppMasterData;
+use App\Repositories\BaseRepository;
+use Illuminate\Database\Eloquent\Builder;
+
+class AppMasterDataRepository extends BaseRepository
+{
+    public function __construct()
+    {
+        parent::__construct(new AppMasterData());
+    }
+    public function getMasters(string $code): Builder
+    {
+        return $this->query()->where('app_master_category_code', $code);
+    }
+
+    public function getMasterById(int $id): AppMasterData
+    {
+        return $this->getById($id);
+    }
+}

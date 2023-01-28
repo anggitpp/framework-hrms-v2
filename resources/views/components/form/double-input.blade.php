5579dbf86d7a2e0080657d80cbada1496bedc001
anggitpp <anggitpp23@gmail.com>
2023-01-28 15:33:47 +0700

    implement validation error in double input

--- resources/views/components/form/double-input.blade.php
+++ resources/views/components/form/double-input.blade.php
@@ -1,12 +1,16 @@
-@props(["label" => "", "required" => "", "name" => "", "placeholder" => "", "value" => "", "name2" => "", "value2" => "", "class" => "", "nospacing" => "", "numeric" => "", "currency" => "", "maxlength" => ""])
+@props(["label" => "", "required" => "", "name" => "", "placeholder" => "", "value" => "", "name2" => "", "value2" => "", "class" => "", "nospacing" => "", "numeric" => "", "currency" => "", "maxlength" => "", "separator" => "s/d"])
 <div class="form-group mb-5">
     <label class="form-label {!! $required ? "required" : "" !!}">{{ $label }}</label>
     <div class="row d-flex align-items-center">
         <div class="col-md-2">
             <input type="text"
                    id="{{ $name }}"
-                   {{ $attributes->merge(['class' => 'form-control w-100px '.$class]) }}
-                   name="{{ $name }}"
+                   @error($name)
+                   {{ $attributes->merge(['class' => 'form-control w-100px is-invalid '.$class]) }}
+                   @else
+                       {{ $attributes->merge(['class' => 'form-control w-100px '.$class]) }}
+                       @enderror
+                       name="{{ $name }}"
                    value="{{ old($name, $currency ? setCurrency($value) : $value) }}"
                    placeholder="{{ $placeholder }}"
                    maxlength="{{ $maxlength }}"
@@ -17,13 +21,16 @@
             />
         </div>
         <div class="col-sm-1">
-            <span class="ms-3 mt-5">s/d</span>
+            <span class="ms-3 mt-5">{!! $separator !!}</span>
         </div>
         <div class="col-md-2">
             <input type="text"
                    id="{{ $name2 }}"
-                   {{ $attributes->merge(['class' => 'form-control w-100px '.$class]) }}
-                   name="{{ $name2 }}"
+                   @error($name2)
+                   {{ $attributes->merge(['class' => 'form-control w-100px is-invalid '.$class]) }}
+                   @else
+                       {{ $attributes->merge(['class' => 'form-control w-100px '.$class]) }}
+                       @enderror                   name="{{ $name2 }}"
                    value="{{ old($name2, $currency ? setCurrency($value2) : $value2) }}"
                    placeholder="{{ $placeholder }}"
                    maxlength="{{ $maxlength }}"
@@ -33,6 +40,15 @@
                 {{ $currency ? 'style=text-align:right;' : '' }}
             />
         </div>
-
     </div>
+    @if($required)
+        @error($name)
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+        <div class="invalid-feedback" id="{{ $name }}-error"></div>
+        @error($name2)
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+        <div class="invalid-feedback" id="{{ $name2 }}-error"></div>
+    @endif
 </div>

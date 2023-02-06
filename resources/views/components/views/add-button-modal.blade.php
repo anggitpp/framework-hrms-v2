2fa37179f4619a718b82b1813e79392291773488
anggitpp <anggitpp23@gmail.com>
2023-02-06 09:58:20 +0700

    add filter in btn modal

--- resources/views/components/views/add-button-modal.blade.php
+++ resources/views/components/views/add-button-modal.blade.php
@@ -1,4 +1,4 @@
-@props(['route' => '', 'text' => '', 'class' => ''])
-<button {{ $attributes->merge(['class' => 'btn btn-primary btn-modal '.$class]) }} data-bs-toggle="modal" data-url="{{ $route }}">
+@props(['route' => '', 'text' => '', 'class' => '', 'filter' => ''])
+<button {{ $attributes->merge(['class' => 'btn btn-primary btn-modal '.$class]) }} data-bs-toggle="modal" data-url="{{ $route }}" data-filter="{{ $filter }}">
     <i class="fa-solid fa-plus"></i> {{ $text }}
 </button>

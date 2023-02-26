f3eb3d8d87a3418db941b14fd02bd128c5988d92
anggitpp <anggitpp23@gmail.com>
2023-02-26 15:37:58 +0700

    implement new controller using helper and service repository

--- app/Http/Controllers/Employee/EmployeeWorkController.php
+++ app/Http/Controllers/Employee/EmployeeWorkController.php
@@ -2,35 +2,32 @@
 
 namespace App\Http\Controllers\Employee;
 
-use App\Exports\GlobalExport;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Employee\EmployeeWorkRequest;
 use App\Imports\Employee\WorkImport;
-use App\Models\Employee\Employee;
-use App\Models\Employee\EmployeeWork;
-use App\Models\Setting\AppMasterData;
+use App\Services\Employee\EmployeeService;
+use App\Services\Employee\EmployeeWorkService;
+use App\Services\Setting\AppMasterDataService;
 use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Facades\Excel;
-use RealRashid\SweetAlert\Facades\Alert;
-use Storage;
 use Str;
-use Yajra\DataTables\DataTables;
 
 class EmployeeWorkController extends Controller
 {
-    public string $workPath;
+    private AppMasterDataService $appMasterDataService;
+    private EmployeeService $employeeService;
+    private EmployeeWorkService $employeeWorkService;
 
     public function __construct()
     {
         $this->middleware('auth');
-        $this->workPath = '/uploads/employee/work/';
+        $this->employeeWorkService = new EmployeeWorkService();
+        $this->appMasterDataService = new AppMasterDataService();
+        $this->employeeService = new EmployeeService();
     }
 
     /**
@@ -40,90 +37,25 @@ class EmployeeWorkController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
+        $positions = $this->appMasterDataService->getMasterForArray('EMP');
+        $ranks = $this->appMasterDataService->getMasterForArray('EP');
+        $grades = $this->appMasterDataService->getMasterForArray('EG');
+        $locations = $this->appMasterDataService->getMasterForArray('ELK');
 
-        $dataMaster = AppMasterData::whereIn('app_master_category_code', ['ELK', 'EP', 'EG', 'EMP'])
-            ->where('status', 't')
-            ->orderBy('app_master_category_code')
-            ->orderBy('order')
-            ->get();
-        $masters = [];
-        foreach ($dataMaster as $key => $value){
-            $masters[$value->app_master_category_code][$value->id] = $value->name;
-        }
-
-        \Session::put('user', $user);
-
-        return view('employees.work.index', compact('masters'));
+        return view(Str::replace('/', '.', $this->menu_path()) . '.index', [
+            'positions' => $positions,
+            'ranks' => $ranks,
+            'grades' => $grades,
+            'locations' => $locations,
+        ]);
     }
 
-    public function data(Request $request){
-        $user = \Session::get('user');
-
-        if($request->ajax()){
-            $filterPosition = $request->get('combo_1');
-            $filterRank = $request->get('combo_2');
-            $filterGrade = $request->get('combo_3');
-            $filterLocation = $request->get('combo_4');
-            $filter = $request->get('search')['value'];
-
-            $table = DB::table('employee_works as t1')
-                ->join('employees as t2', 't1.employee_id', 't2.id')
-                ->join('employee_positions as t3', function ($join){
-                    $join->on('t2.id', 't3.employee_id');
-                    $join->where('t3.status', 't');
-                })
-                ->select([
-                    't1.id',
-                    't1.employee_id',
-                    't1.company',
-                    't1.position',
-                    't1.start_date',
-                    't1.end_date',
-                    't1.filename',
-                    't2.name as employee_name',
-                    't2.employee_number',
-                    't3.position_id',
-                    't3.rank_id',
-                    't3.grade_id',
-                    't3.location_id',
-                ]);
-
-            if(!$user->hasPermissionTo('lvl3 '.$this->menu_path()))
-                $table->where('t3.leader_id', $user->employee_id);
-
-            return DataTables::of($table)
-                ->filter(function ($query) use ($filter, $filterGrade, $filterPosition, $filterRank, $filterLocation) {
-                    if (isset($filter)) $query->where('t2.name', 'like', "%{$filter}%")
-                        ->orWhere('t2.employee_number', 'like', "%{$filter}%")
-                        ->orWhere('t1.company', 'like', "%{$filter}%");
-                    if (isset($filterPosition)) $query->where('position_id', $filterPosition);
-                    if (isset($filterRank)) $query->where('rank_id', $filterRank);
-                    if (isset($filterGrade)) $query->where('grade_id', $filterGrade);
-                    if (isset($filterLocation)) $query->where('location_id', $filterLocation);
-                })
-                ->editColumn('start_date', function ($model) {
-                    return $model->start_date ? setDate($model->start_date) : '';
-                })
-                ->editColumn('end_date', function ($model) {
-                    return $model->end_date ? setDate($model->end_date) : '';
-                })
-                ->editColumn('filename', function ($model) {
-                    return $model->filename ? view('components.datatables.download', [
-                        'url' => $model->filename
-                    ]) : '';
-                })
-                ->addColumn('action', function ($model) {
-                    return view('components.views.action', [
-                        'menu_path' => $this->menu_path(),
-                        'url_edit' => route(str_replace('/', '.', $this->menu_path()).'.edit', $model->id),
-                        'url_destroy' => route(str_replace('/', '.', $this->menu_path()).'.destroy', $model->id),
-                        'isModal' => false,
-                    ]);
-                })
-                ->addIndexColumn()
-                ->make();
-        }
+    /**
+     * @throws Exception
+     */
+    public function data(Request $request)
+    {
+        return $this->employeeWorkService->data($request);
     }
 
     /**
@@ -133,17 +65,9 @@ class EmployeeWorkController extends Controller
      */
     public function create()
     {
-        $user = \Session::get('user');
-
-        $data['employees'] = Employee::select(['name', 'id', 'employee_number', DB::raw("CONCAT(employee_number, ' - ', name) as namaPegawai")])
-            ->whereHas('position', function ($query) use ($user) {
-                if(!$user->hasPermissionTo('lvl3 '.$this->menu_path())) $query->where('leader_id', $user->employee_id);
-            })
-            ->orderBy('name')
-            ->pluck("namaPegawai", 'id')
-            ->toArray();
-
-        return view('employees.work.form', $data);
+        return view(Str::replace('/', '.', $this->menu_path()) . '.form', [
+            'employees' => $this->employeeService->getEmployeesForArray(),
+        ]);
     }
 
     /**
@@ -154,58 +78,21 @@ class EmployeeWorkController extends Controller
      */
     public function store(EmployeeWorkRequest $request)
     {
-        try {
-            $filename = '';
-            if($request->hasFile('filename')) {
-                $resize = false;
-                $extension = $request->file('filename')->extension();
-                if ($extension == 'png' || $extension == 'jpg' || $extension == 'jpeg') $resize = true;
-
-                $filename = uploadFile(
-                    $request->file('filename'),
-                    'employee-work_' . Str::slug($request->input('company')) . '_' . time(),
-                    $this->workPath, $resize);
-            }
-            $request->merge(['start_date' => resetDate($request->input('start_date'))]);
-            $request->merge(['end_date' => $request->input('end_date') ? resetDate($request->input('end_date')) : '']);
-
-            $work = EmployeeWork::create($request->except('filename'));
-            $work->filename = $filename;
-            $work->save();
-
-            Alert::success('Success', 'Data berhasil disimpan');
-
-            return redirect()->route(Str::replace('/', '.', $this->menu_path()).'.index');
-        } catch (Exception $e) {
-
-            DB::rollBack();
-
-            Alert::error('Error', $e->getMessage());
-
-            return redirect()->route(Str::replace('/', '.', $this->menu_path()).'.index');
-        }
+        return submitDataHelper($this->employeeWorkService->saveWork($request));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return Application|Factory|View
      */
     public function edit(int $id)
     {
-        $user = \Session::get('user');
-
-        $data['work'] = EmployeeWork::findOrFail($id);
-        $data['employees'] = Employee::select(['name', 'id', 'employee_number', DB::raw("CONCAT(employee_number, ' - ', name) as namaPegawai")])
-            ->whereHas('position', function ($query) use ($user) {
-                if(!$user->hasPermissionTo('lvl3 '.$this->menu_path())) $query->where('leader_id', $user->employee_id);
-            })
-            ->orderBy('name')
-            ->pluck("namaPegawai", 'id')
-            ->toArray();
-
-        return view('employees.work.form', $data);
+        return view(Str::replace('/', '.', $this->menu_path()) . '.form', [
+            'work' => $this->employeeWorkService->getWorkById($id),
+            'employees' => $this->employeeService->getEmployeesForArray(),
+        ]);
     }
 
     /**
@@ -217,181 +104,36 @@ class EmployeeWorkController extends Controller
      */
     public function update(EmployeeWorkRequest $request, int $id)
     {
-        try {
-            $work = EmployeeWork::findOrFail($id);
+        return submitDataHelper($this->employeeWorkService->saveWork($request, $id));
 
-            if($request->get('isDelete') == 't'){
-                deleteFile($this->workPath.$work->filename);
-                $work->update([
-                    'filename' => null,
-                ]);
-            }
-            if ($request->hasFile('filename')) {
-                $resize = false;
-                $extension = $request->file('filename')->extension();
-                if ($extension == 'png' || $extension == 'jpg' || $extension == 'jpeg') $resize = true;
-
-                $filename = uploadFile(
-                    $request->file('filename'),
-                    'employee-work_' . Str::slug($request->input('company')) . '_' . time(),
-                    $this->workPath, $resize);
-
-                $work->update([
-                    'filename' => $filename,
-                ]);
-            }
-
-            $request->merge(['start_date' => resetDate($request->input('start_date'))]);
-            $request->merge(['end_date' => $request->input('end_date') ? resetDate($request->input('end_date')) : '']);
-
-            $work->update($request->except('filename'));
-
-            Alert::success('Success', 'Data berhasil disimpan');
-
-            return redirect()->route(Str::replace('/', '.', $this->menu_path()).'.index');
-        } catch (Exception $e) {
-
-            DB::rollBack();
-
-            Alert::error('Error', $e->getMessage());
-
-            return redirect()->route(Str::replace('/', '.', $this->menu_path()).'.index');
-        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return RedirectResponse
      */
     public function destroy(int $id)
     {
-        try {
-            $work = EmployeeWork::findOrFail($id);
-            if(Storage::exists($this->workPath.$work->filename)) Storage::delete($this->workPath.$work->filename);
-            $work->delete();
-
-            DB::commit();
-
-            Alert::success('Success', 'Data berhasil dihapus');
-
-            return redirect()->back();
-        } catch (Exception $e) {
-
-            DB::rollBack();
-
-            Alert::error('Error', $e->getMessage());
-
-            return redirect()->back();
-        }
+        return deleteDataHelper($this->employeeWorkService->deleteWork($id));
     }
 
     public function export(Request $request)
     {
-        $user = \Session::get('user');
-
-        $categories = AppMasterData::whereAppMasterCategoryCode('EKPL')
-            ->pluck('name', 'id')
-            ->toArray();
-        $types = AppMasterData::whereAppMasterCategoryCode('ETPL')
-            ->pluck('name', 'id')
-            ->toArray();
-
-        $filterPosition = $request->get('combo_1');
-        $filterRank = $request->get('combo_2');
-        $filterGrade = $request->get('combo_3');
-        $filterLocation = $request->get('combo_4');
-        $filter = $request->get('filter');
-
-        $sql = DB::table('employee_works as t1')
-            ->join('employees as t2', 't1.employee_id', 't2.id')
-            ->join('employee_positions as t3', function ($join){
-                $join->on('t2.id', 't3.employee_id');
-                $join->where('t3.status', 't');
-            })
-            ->select([
-                't1.id',
-                't1.employee_id',
-                't1.company',
-                't1.position',
-                't1.city',
-                't1.job_desc',
-                't1.start_date',
-                't1.end_date',
-                't2.name as employee_name',
-                't2.employee_number',
-            ]);
-        if($filterPosition) $sql->where('t3.position_id', $filterPosition);
-        if($filterRank) $sql->where('t3.rank_id', $filterRank);
-        if($filterGrade) $sql->where('t3.grade_id', $filterGrade);
-        if($filterLocation) $sql->where('t3.location_id', $filterLocation);
-        if($filter) $sql->where(function ($query) use ($filter) {
-            $query->where('t1.subject', 'like', '%'.$filter.'%')
-                ->orWhere('t2.name', 'like', '%'.$filter.'%')
-                ->orWhere('t2.employee_number', 'like', '%'.$filter.'%');
-        });
-
-        if(!$user->hasPermissionTo('lvl3 '.$this->menu_path()))
-            $sql->where('t3.leader_id', $user->employee_id);
-
-        $data = [];
-        $works = $sql->get();
-        foreach ($works as $k => $work){
-            $data[] = [
-                $k + 1,
-                $work->employee_number." ",
-                $work->employee_name,
-                $work->company,
-                $work->position,
-                setDate($work->start_date),
-                $work->end_date ? setDate($work->end_date) : '',
-                $work->city,
-                $work->job_desc,
-            ];
-        }
-
-        $columns = ["no", "data pegawai" => ["nip", "nama"], "data riwayat kerja" => ["perusahaan", "posisi", "tanggal mulai", "tanggal selesai", "kota", "keterangan"]];
-
-        $widths = [10, 20, 30, 30, 30, 20, 20, 20, 50];
-
-        $aligns = ['center', 'center', 'left', 'left', 'left', 'center', 'center'];
-
-        return Excel::download(new GlobalExport(
-            [
-                'columns' => $columns,
-                'widths' => $widths,
-                'aligns' => $aligns,
-                'data' => $data,
-                'title' => 'Data Riwayat Kerja',
-            ]
-        ), 'Data Riwayat Kerja.xlsx');
+        return $this->employeeWorkService->exportWork($request);
     }
 
     public function import()
     {
         return view('components.form.import-form', [
             'menu_path' => $this->menu_path(),
-            'title' => 'Import Data Riwayat Kerja',
+            'title' => 'Data Import Kerja',
         ]);
     }
 
     public function processImport(Request $request)
     {
-        try {
-            if($request->hasFile('filename')) {
-                Excel::import(new WorkImport(), $request->file('filename'));
-
-                return response()->json([
-                    'success' => 'Data Riwayat Kerja selesai diimport',
-                    'url' => route(Str::replace('/', '.', $this->menu_path()) . '.index'),
-                ]);
-            }
-        } catch (Exception $e) {
-            return response()->json([
-                'success' => 'Gagal ' . $e->getMessage(),
-                'url' => route(Str::replace('/', '.', $this->menu_path()) . '.index'),
-            ]);
-        }
+        return importHelper(new WorkImport(), $request);
     }
 }

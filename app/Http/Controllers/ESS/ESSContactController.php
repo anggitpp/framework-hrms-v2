f167bf6b14ae69f2fde5c0809095a11f080e246e
anggitpp <anggitpp23@gmail.com>
2023-03-09 12:30:08 +0700

    implement service repository pattern in contact ess

--- app/Http/Controllers/ESS/ESSContactController.php
+++ app/Http/Controllers/ESS/ESSContactController.php
@@ -4,28 +4,30 @@ namespace App\Http\Controllers\ESS;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Employee\EmployeeContactRequest;
-use App\Models\Employee\Employee;
-use App\Models\Employee\EmployeeContact;
-use App\Models\ESS\EssTimesheet;
-use App\Models\Setting\AppMasterData;
-use Carbon\Carbon;
+use App\Services\Employee\EmployeeContactService;
+use App\Services\Employee\EmployeeService;
+use App\Services\Setting\AppMasterDataService;
 use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use RealRashid\SweetAlert\Facades\Alert;
 use Str;
-use Yajra\DataTables\DataTables;
 
 class ESSContactController extends Controller
 {
+    private EmployeeService $employeeService;
+    private EmployeeContactService $employeeContactService;
+    private AppMasterDataService $appMasterDataService;
+
     public function __construct()
     {
         $this->middleware('auth');
+        $this->employeeService = new EmployeeService();
+        $this->employeeContactService = new EmployeeContactService();
+        $this->appMasterDataService = new AppMasterDataService();
     }
 
     /**
@@ -35,92 +37,50 @@ class ESSContactController extends Controller
      */
     public function index()
     {
-        $employee = Employee::find(Auth::user()->employee_id);
-        $employee->position->location_id = AppMasterData::find($employee->position->location_id)->name ?? '';
-        $employee->position->position_id = AppMasterData::find($employee->position->position_id)->name ?? '';
-        $employee->position->grade_id = AppMasterData::find($employee->position->grade_id)->name ?? '';
-        $employee->position->unit_id = AppMasterData::find($employee->position->unit_id)->name ?? '';
+        $employee = $this->employeeService->getEmployeeById(Auth::user()->employee_id);
 
-        return view('ess.contact.index', compact('employee'));
-    }
-
-    public function data(Request $request)
-    {
-        $relationships = AppMasterData::whereAppMasterCategoryCode('EHK')->pluck('name', 'id')->toArray();
-        if($request->ajax()){
-            $filter = $request->get('search')['value'];
-            return DataTables::of(EmployeeContact::select(['id', 'relationship_id', 'name', 'phone_number', 'description'])
-                ->where('employee_id', Auth::user()->employee_id))
-                ->filter(function ($query) use ($filter) {
-                    $query->where(function ($query) use ($filter) {
-                        $query->where('name', 'like', "%$filter%")
-                            ->orWhere('phone_number', 'like', "%$filter%");
-                    });
-                })
-                ->editColumn('relationship_id', function ($data) use ( $relationships) {
-                    return $relationships[$data->relationship_id];
-                })
-                ->addColumn('action', function ($model) {
-                    return view('components.views.action', [
-                        'menu_path' => $this->menu_path(),
-                        'url_edit' => route(Str::replace('/', '.', $this->menu_path()).'.edit', $model->id),
-                        'url_destroy' => route(Str::replace('/', '.', $this->menu_path()).'.destroy', $model->id),
-                    ]);
-                })
-                ->addIndexColumn()
-                ->make();
-        }
+        return view(Str::replace('/', '.', $this->menu_path()) . '.index', compact('employee'));
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Application|Factory|View
+     * @throws Exception
      */
+    public function data(Request $request)
+    {
+        return $this->employeeContactService->data($request, true);
+    }
+
     public function create()
     {
-        $relationships = AppMasterData::whereAppMasterCategoryCode('EHK')->pluck('name', 'id')->toArray();
-
-        return view('ess.contact.form', compact('relationships'));
+        return view(Str::replace('/', '.', $this->menu_path()) . '.form', [
+            'relationships' => $this->appMasterDataService->getMasterForArray('EHK'),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param EmployeeContactRequest $request
-     * @return JsonResponse
+     * @return RedirectResponse
      */
     public function store(EmployeeContactRequest $request)
     {
-        try {
-            EmployeeContact::create($request->all());
-
-            return response()->json([
-                'success'=>'Data Kontak berhasil disimpan',
-                'url'=> route(Str::replace('/', '.', $this->menu_path()).'.index')
-            ]);
-        } catch (Exception $e) {
-            return response()->json([
-                'success'=>'Gagal, '.$e->getMessage(),
-                'url'=> route(Str::replace('/', '.', $this->menu_path()).'.index')
-            ]);
-        }
+        return submitDataHelper(function () use ($request) {
+            $this->employeeContactService->saveContact($request);
+        }, true);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return Application|Factory|View
      */
     public function edit(int $id)
     {
-        $contact = EmployeeContact::findOrFail($id);
-        $relationships = AppMasterData::whereAppMasterCategoryCode('EHK')->pluck('name', 'id')->toArray();
-
-        return view('ess.contact.form', [
-            'contact' => $contact,
-            'relationships' => $relationships
+        return view(Str::replace('/', '.', $this->menu_path()) . '.form', [
+            'contact' => $this->employeeContactService->getContactById($id),
+            'relationships' => $this->appMasterDataService->getMasterForArray('EHK'),
         ]);
     }
 
@@ -129,47 +89,25 @@ class ESSContactController extends Controller
      *
      * @param EmployeeContactRequest $request
      * @param int $id
-     * @return JsonResponse
+     * @return RedirectResponse
      */
     public function update(EmployeeContactRequest $request, int $id)
     {
-        try {
-            $contact = EmployeeContact::findOrFail($id);
-            $contact->update($request->all());
-
-            return response()->json([
-                'success'=>'Data Kontak berhasil disimpan',
-                'url'=> route(Str::replace('/', '.', $this->menu_path()).'.index')
-            ]);
-        } catch (Exception $e) {
-            return response()->json([
-                'success'=>'Gagal, '.$e->getMessage(),
-                'url'=> route(Str::replace('/', '.', $this->menu_path()).'.index')
-            ]);
-        }
-
+        return submitDataHelper(function () use ($request, $id) {
+            $this->employeeContactService->saveContact($request, $id);
+        }, true);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return RedirectResponse
      */
     public function destroy(int $id)
     {
-        try {
-            $contact = EmployeeContact::findOrFail($id);
-            $contact->delete();
-
-            Alert::success('Success', 'Data berhasil dihapus');
-
-            return redirect()->back();
-        } catch (Exception $e) {
-
-            Alert::error('Error', $e->getMessage());
-
-            return redirect()->back();
-        }
+        return deleteDataHelper(function () use ($id) {
+            $this->employeeContactService->deleteContact($id);
+        });
     }
 }

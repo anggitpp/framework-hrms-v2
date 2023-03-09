556713381e79822e43772d059282673228af7a8f
anggitpp <anggitpp23@gmail.com>
2023-03-09 12:30:42 +0700

    add some function relationship in employee model

--- app/Models/Employee/Employee.php
+++ app/Models/Employee/Employee.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\Employee;
 
+use App\Models\Setting\AppMasterData;
 use App\Models\Setting\AppParameter;
 use App\Traits\Blameable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -34,6 +36,13 @@ class Employee extends Model
         'email',
         'gender',
         'attendance_pin',
+        'npwp_number',
+        'npwp_date',
+        'bpjs_date',
+        'bpjs_number',
+        'bpjs_tk_number',
+        'bpjs_tk_date',
+        'blood_type',
     ];
 
     public function position(): HasOne
@@ -44,6 +53,11 @@ class Employee extends Model
         return $position;
     }
 
+    public function bank(): HasOne
+    {
+        return $this->hasOne(EmployeeBank::class);
+    }
+
     public function positions(): HasMany
     {
         return $this->hasMany(EmployeePosition::class);
@@ -52,5 +66,20 @@ class Employee extends Model
     public function scopeActive($query)
     {
         return $query->where('status_id', AppParameter::whereCode('SAP')->value('value'));
+    }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(AppMasterData::class, 'status_id');
+    }
+
+    public function maritalStatus(): BelongsTo
+    {
+        return $this->belongsTo(AppMasterData::class, 'marital_status_id');
+    }
+
+    public function religion(): BelongsTo
+    {
+        return $this->belongsTo(AppMasterData::class, 'religion_id');
     }
 }
